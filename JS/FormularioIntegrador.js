0d1c4d5d5233400b491df61a2eb2f1cfe19e2d98
Anie <santiagonazarit789@gmail.com>
2026-03-02 23:56:21 -0500

    logo

--- JS/FormularioIntegrador.js
+++ JS/FormularioIntegrador.js
@@ -1,10 +1,10 @@
-document.getElementById("imagen").addEventListener("change", function (event) {
+document.getElementById("imagen").addEventListener("change", function(event) {
     let file = event.target.files[0];
-    let preview = document.getElementById("preview");
+    let preview = document.getElementById("preview-img");
 
     if (file) {
         let reader = new FileReader();
-        reader.onload = function (e) {
+        reader.onload = function(e) {
             preview.src = e.target.result;
             preview.style.display = "block";
         }
@@ -12,31 +12,34 @@ document.getElementById("imagen").addEventListener("change", function (event) {
     } else {
         preview.src = "";
         preview.style.display = "none";
-    }
+    }   
 });
 
-document.getElementById("form.product").addEventListener("submit", function (event) {
+
+document.getElementById("form-product").addEventListener("submit", function(event) {
     event.preventDefault();
 
     let nombre = document.getElementById("nombre").value.trim();
     let precio = document.getElementById("precio").value.trim();
-    let catergoria = document.getElementById("categoria").value;
+    let marca = document.getElementById("marca").value.trim();
+    let categoria = document.getElementById("categoria").value;
+    let descripcion = document.getElementById("descripcion").value.trim();
     let imagenInput = document.getElementById("imagen");
-    let imagen = document.getElementById("previewImg").src;
+ 
+    let imagen = document.getElementById("preview-img").src;
 
     if (imagenInput.files.length === 0) {
         alert("Por favor, selecciona una imagen.");
         return;
     }
 
-
-    if (nombre === "" || precio === "" || catergoria === "") {
+    if (nombre === "" || precio === "" || marca === "" || categoria === "" || descripcion === "") {
         alert("Por favor, completa todos los campos.");
         return;
     }
 
     let productos = JSON.parse(localStorage.getItem("productos")) || [];
-    productos.push({ nombre, precio, catergoria, imagen });
+    productos.push({ nombre, precio, marca, categoria, descripcion, imagen });
 
     localStorage.setItem("productos", JSON.stringify(productos));
     alert("Producto agregado exitosamente.");
